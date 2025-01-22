dbbbbda2a3d6be354c20be8031c917cf406c5877
10sujitkhanal <10sujitkhanal@gmail.com>
2025-01-22 13:26:46 +0545

    Updated Template

--- khanalsujit/settings.py
+++ khanalsujit/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'khanalsujit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': "django.db.backends.postgresql",
-#         'HOST': "100.42.187.204",
-#         'NAME': "sujitdb1",
-#         'USER': "sujitdb1",
-#         'PASSWORD': "2i5D5Mzb5EeE",
-#         'PORT': "5432",
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql",
+        'HOST': "100.42.187.204",
+        'NAME': "khanalsujit",
+        'USER': "khanalsujit",
+        'PASSWORD': "iZe8sxpmFPwZ",
+        'PORT': "5432",
+    }
+}
 
 
 # Password validation
